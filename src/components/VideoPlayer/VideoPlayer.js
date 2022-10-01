4d98aff279157d837847ea717f9c3d01a0d65f85
ShivamKun <shivammovefl@gmail.com>
2022-10-01 07:55:36 +0530

    `Palette Fix`

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -212,7 +212,7 @@ function VideoPlayer({ sources, internalPlayer, setInternalPlayer, title }) {
       style={{
         marginBottom: "1rem",
         fontFamily: '"Gilroy-Medium", sans-serif',
-        "--plyr-color-main": "#991111",
+        "--plyr-color-main": "#303030",
       }}
     >
       <Conttainer>
