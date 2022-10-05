6eb7e9c01f980cfaf6d68e82d9a9c610525ff2f0
Kayla Kremer <kkremer@getharvest.com>
2022-10-05 09:54:57 -0400

    fix: class to className in PageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -4,11 +4,11 @@ import styled from "styled-components";
 
 function PageNotFound() {
     return (
-        <div class="pageNotFound">
+        <div className="pageNotFound">
                         <img src="https://cdn.discordapp.com/attachments/928341740434378752/1016160204355813437/3.png" width="320" alt="Akio" ></img>
-                        <div class="errorMessage">
-                            <p class="err">ERROR</p>
-                            <p class="num"> 404</p>
+                        <div className="errorMessage">
+                            <p className="err">ERROR</p>
+                            <p className="num"> 404</p>
                             <h1>Looks like you are lost...</h1>
                         </div>
         </div>
